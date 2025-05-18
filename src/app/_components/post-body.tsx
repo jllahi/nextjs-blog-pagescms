caf0eb7fdfa4493780ebf85d23db3dc3e260fe4b
Jordi Llahi <jllahi@Mac-mini-de-Jordi.local>
2025-05-18 03:00:05 +0200

    Update tailwind

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export function PostBody({ content }: Props) {
   return (
-    <div className="max-w-2xl mx-auto">
+    <div className="prose max-w-2xl mx-auto">
       <div
         className={markdownStyles["markdown"]}
         dangerouslySetInnerHTML={{ __html: content }}
